37f4227835b7e46c47293f16584a3a762750d930
chandinirayadurgam <44672808+chandinirayadurgam@users.noreply.github.com>
2025-02-14 12:44:18 -0500

    Create ProductController.java

---
+++ ProductController.java
@@ -0,0 +1,26 @@
+package com.ecommerce.controller;
+
+import com.ecommerce.model.Product;
+import com.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+}
